409f9f5ef93bf998562d0b06fa21eef0544dea2f
unionelein <suvorov1995@bk.ru>
2019-04-11 22:11:33 +0300

    update gallery part 1

--- public/js/event_party/event_party.js
+++ public/js/event_party/event_party.js
@@ -1,4 +1,12 @@
 $(document).ready(() => {
     new Chat();
     new Announcement();
+
+    const gallery = document.getElementById('event-gallery');
+    lightGallery(gallery, {
+        videojs: true,
+        thumbnail:true,
+        animateThumb: false,
+        showThumbByDefault: false
+    });
 });
