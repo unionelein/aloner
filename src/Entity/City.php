6c8c8151a09a6cc8bc32a9a4532c54400ef26f0c
Aliaksei Suvorau <asuvorau@itsupportme.com>
2019-10-04 19:44:01 +0300

    update all

--- src/Entity/City.php
+++ src/Entity/City.php
@@ -2,8 +2,6 @@
 
 namespace App\Entity;
 
-use Doctrine\Common\Collections\ArrayCollection;
-use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
@@ -12,79 +10,50 @@ use Doctrine\ORM\Mapping as ORM;
 class City
 {
     /**
+     * @var int
+     *
      * @ORM\Id()
      * @ORM\GeneratedValue()
-     * @ORM\Column(type="integer", name="City_Id")
+     * @ORM\Column(type="integer", name="city_id")
      */
     private $id;
 
     /**
-     * @ORM\Column(type="string", length=255)
+     * @var string
+     *
+     * @ORM\Column(type="string", name="city_name", length=255)
      */
     private $name;
 
     /**
-     * @ORM\OneToMany(targetEntity="App\Entity\User", mappedBy="city")
+     * @param string $name
      */
-    private $users;
-
-    /**
-     * @ORM\OneToMany(targetEntity="App\Entity\Event", mappedBy="city")
-     */
-    private $events;
-
     public function __construct(string $name)
     {
         $this->name = $name;
-        $this->users = new ArrayCollection();
-        $this->events = new ArrayCollection();
     }
 
-    public function getId(): ?int
+    /**
+     * @return int
+     */
+    public function getId(): int
     {
         return $this->id;
     }
 
+    /**
+     * @return string
+     */
     public function getName(): string
     {
         return $this->name;
     }
 
-    public function setName(string $name): self
-    {
-        $this->name = $name;
-
-        return $this;
-    }
-
     /**
-     * @return Collection|User[]
+     * @return string
      */
-    public function getUsers(): Collection
-    {
-        return $this->users;
-    }
-
     public function __toString(): string
     {
         return $this->name;
-    }
-
-    /**
-     * @return Collection|Event[]
-     */
-    public function getEvents(): Collection
-    {
-        return $this->events;
-    }
-
-    public function addEvent(Event $event): self
-    {
-        if (!$this->events->contains($event)) {
-            $this->events[] = $event;
-            $event->setCity($this);
-        }
-
-        return $this;
     }
 }
