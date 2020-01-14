5278e463aa631d3ab1cef4b744b75ed79649e002
asuvorov <unionelein@gmail.com>
2020-01-14 22:35:01 +0300

    added script

---
+++ .docker/start.sh
@@ -0,0 +1,12 @@
+cd ..
+
+printf 'Up docker services...\n'
+docker-compose up -d
+
+printf 'Install composer...\n'
+docker-compose exec 'php-fpm' sh -c 'composer install'
+
+printf 'Up database...\n'
+docker-compose exec 'php-fpm' sh -c 'php bin/console doctrine:database:create --if-not-exists'
+docker-compose exec 'php-fpm' sh -c 'php bin/console doctrine:migrations:migrate --no-interaction --allow-no-migration'
+
